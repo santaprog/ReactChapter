bafbe593c265a32846b80f7950d2d44a1b112207
Prajakta <sangaonkarprajakta@gmail.com>
2025-12-03 12:10:35 +0530

    Removed comments and import React

--- App.js
+++ App.js
@@ -1,30 +1,7 @@
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading", xyz: "abc" },
-//     "Hello world from React!");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 
-/*
-<div id= "parent">
-    <div id= "child">
-        <h1>Iam first tag</h1>
-        <h2>Iam first tag</h2>
-    </div>   
-    <div id= "child2">
-        <h1>Iam first tag</h1>
-        <h2>Iam first tag</h2>
-    </div>  
-</div>
-
-*/
-
-const parent = React.createElement("div", { id: "parent" },
- [React.createElement("div", { id: "child" }, 
- [React.createElement("h1", {}, "Iam first tag"),React.createElement("h2", {}, "Iam second tag")]),React.createElement("div", { id: "child2" }, 
- [React.createElement("h1", {}, "Iam new one tag"),React.createElement("h2", {}, "Iam new second tag")])]);
-
-//parexnt is object we need to render in root element
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("h1", {}, "React first tag");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
